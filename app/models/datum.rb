f04c3c83c13c9af2c83fc39413901d6babb88910
satoru-koyama <satoru123.fly.9.26@gmail.com>
2021-06-28 11:41:25 +0000

    [Add] dataコントローラ, usersコントローラ, 関連付け, ルーティング

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -1,2 +1,5 @@
 class Datum < ApplicationRecord
+
+  belongs_to :user
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
+  resorces :users, only: [:show, :edit]
+  resorces :data, except:[:destroy]
 end
