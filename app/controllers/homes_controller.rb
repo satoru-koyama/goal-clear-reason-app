2936271f33016a43d5c766b4c0c968b87524ab71
satoru-koyama <satoru123.fly.9.26@gmail.com>
2021-06-29 09:07:57 +0000

    [Add] homes#home, homes/home.html.erb, data/edit.html.erb

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,6 +4,15 @@ class HomesController < ApplicationController
   end
 
   def home
+    require "date"
+    # 今日の日付のDateクラスのオブジェクトを作成する
+    today = Date.today
+    # 昨日の日付のdateクラスのオブジェクトを作成する
+    yesterday = today - 1
+    # 今日の日付と同じ日付を持っているレコードを扱うDatumクラスのインスタンスを生成する
+    @datum_today = Datum.find_by(date: today)
+    # 昨日の日付と同じ日付を持っているレコードを扱うDatumクラスのインスタンスを生成する
+    @datum_yesterday = Datum.find_by(date: yesterday)
   end
 
 end
